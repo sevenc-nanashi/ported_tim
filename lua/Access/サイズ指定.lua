7f2026fcdbd993699ed75eb4d77f981e01bcaf66
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-03-06 00:52:38 +0900

    feat: サイズ修正T.anmを実装

--- lua/Access/サイズ指定.lua
+++ lua/Access/サイズ指定.lua
@@ -3,30 +3,30 @@
 ---min=0
 ---max=5000
 ---step=0.1
-local rename_me_track0 = 320
+local width = 320
 
 ---$track:縦
 ---min=0
 ---max=5000
 ---step=0.1
-local rename_me_track1 = 180
+local height = 180
 
 ---$track:基準X[%]
 ---min=-100
 ---max=100
 ---step=0.1
-local rename_me_track2 = 0
+local center_x = 0
 
 ---$track:基準Y[%]
 ---min=-100
 ---max=100
 ---step=0.1
-local rename_me_track3 = 0
+local center_y = 0
 
-local ow = rename_me_track0 * 0.5
-local oh = rename_me_track1 * 0.5
-local cx = ow * rename_me_track2 * 0.01
-local cy = oh * rename_me_track3 * 0.01
+local ow = width * 0.5
+local oh = height * 0.5
+local cx = ow * center_x * 0.01
+local cy = oh * center_y * 0.01
 
 obj.setoption("drawtarget", "tempbuffer", 2 * ow + 2, 2 * oh + 2)
 obj.drawpoly(-ow, -oh, 0, ow, -oh, 0, ow, oh, 0, -ow, oh, 0)
--- lua/Access/領域サイズ指定+事前拡大回転.lua
+++ lua/Access/領域サイズ指定+事前拡大回転.lua
@@ -3,26 +3,26 @@
 ---min=0
 ---max=5000
 ---step=0.1
-local rename_me_track0 = 300
+local width = 300
 
 ---$track:高さ
 ---min=0
 ---max=5000
 ---step=0.1
-local rename_me_track1 = 300
+local height = 300
 
 ---$track:回転角
 ---min=-3600
 ---max=3600
 ---step=0.1
-local rename_me_track2 = 0
+local angle = 0
 
 ---$track:拡大率
 ---min=0
 ---max=5000
 ---step=0.1
-local rename_me_track3 = 100
+local scale = 100
 
-obj.setoption("drawtarget", "tempbuffer", rename_me_track0, rename_me_track1)
-obj.draw(0, 0, 0, rename_me_track3 * 0.01, 1, 0, 0, rename_me_track2)
+obj.setoption("drawtarget", "tempbuffer", width, height)
+obj.draw(0, 0, 0, scale * 0.01, 1, 0, 0, angle)
 obj.load("tempbuffer")
