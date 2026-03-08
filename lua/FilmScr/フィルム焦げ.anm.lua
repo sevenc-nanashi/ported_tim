466c09eb8765e32f9e7cae8b7e5d84b3d837ff21
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-03-08 19:17:19 +0900

    feat: フィルム焦げを移植

--- lua/FilmScr/フィルム焦げ.anm.lua
+++ lua/FilmScr/フィルム焦げ.anm.lua
@@ -5,7 +5,7 @@
 ---step=0.1
 local track_burn = 30
 
----$track:輪郭ﾎﾞｶｼ
+---$track:輪郭ぼかし
 ---min=0
 ---max=100
 ---step=0.1
@@ -23,26 +23,41 @@ local track_radius = 50
 ---step=0.1
 local track_random_seed = 0
 
----$value:発火点集中
+---$track:発火点集中
+---min=0
+---max=1000
+---step=0.1
 local kei = 50
 
 ---$color:燃焼色
 local col = 0x000000
 
----$value:燃焼ぼかし(%)
+---$track:燃焼ぼかし(%)
+---min=0
+---max=100
+---step=0.1
 local fibl = 5
 
----$value:位置ズレ(%)
+---$track:位置ズレ(%)
+---min=0
+---max=100
+---step=0.1
 local porn = 5
 
----$value:淵発光
+---$track:縁発光
+---min=0
+---max=1000
+---step=1
 local lst = 300
 
----$value:淵発光拡散(%)
+---$track:縁発光拡散(%)
+---min=0
+---max=100
+---step=0.1
 local lsig = 7.5
 
----$check:淵発光
-local check0 = false
+---$check:縁発光
+local glow_edge = false
 
 local burn = track_burn * 0.01
 local blur = track_contour_blur * 0.01
@@ -53,9 +68,12 @@ porn = porn * 0.01
 fibl = fibl * 0.01
 lsig = lsig * 0.01
 
+-- NOTE: AviUtl2では、alpha_subを複数回かけるとバグるため、マスクのバッファを作ってそこに描画してから合成する
+
+obj.copybuffer("cache:original", "object")
+
 local w, h = obj.getpixel()
 obj.setoption("drawtarget", "tempbuffer", w, h)
-obj.draw()
 
 dw = math.max(w, h)
 dr = fiR * dw
@@ -70,23 +88,31 @@ for i = -5, 5 do
         if size < 0 then
             size = 0
         end
-        obj.setoption("blend", "alpha_sub")
         dx = obj.rand(-dd, dd, i, j + 1000 + seed)
         dy = obj.rand(-dd, dd, i, j + 2000 + seed)
         obj.draw(i * dw * 0.1 + dx, j * dw * 0.1 + dy, 0, size * 0.01)
     end
 end
 
+obj.copybuffer("cache:mask", "tempbuffer")
+obj.copybuffer("tempbuffer", "cache:original")
+obj.copybuffer("object", "cache:mask")
+obj.setoption("blend", "alpha_sub")
+obj.draw()
 obj.load("tempbuffer")
-obj.setoption("blend", 0)
+obj.setoption("blend", "none")
 obj.setoption("drawtarget", "tempbuffer", w, h)
 obj.effect("縁取り", "サイズ", 1, "ぼかし", fibl * dw, "color", col)
 obj.draw()
 obj.load("tempbuffer")
 
-if check0 then
+if glow_edge then
+    -- AviUtl2では逆光で縁が光ってしまうため、領域拡張+クリッピングで光る部分を外に出す
+    local size = dw * lsig
     obj.setoption("drawtarget", "tempbuffer", w, h)
+    obj.effect("領域拡張", "塗りつぶし", 1, "上", size, "下", size, "左", size, "右", size)
     obj.effect("ライト", "強さ", lst, "拡散", dw * lsig, "逆光", 1)
+    obj.effect("クリッピング", "上", size, "下", size, "左", size, "右", size)
     obj.draw()
     obj.load("tempbuffer")
 end
