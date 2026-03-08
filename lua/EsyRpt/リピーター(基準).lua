7fc53c1b6a4339a21f35e294cced915065936151
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-03-08 18:19:20 +0900

    feat: 簡易リピーターを移植

--- lua/EsyRpt/リピーター(基準).lua
+++ lua/EsyRpt/リピーター(基準).lua
@@ -1,15 +1,20 @@
 --label:tim2\配置\簡易リピーター.anm
----$track:X
----min=-5000
----max=5000
----step=0.1
-local track_x = 300
+-- ---$track:X
+-- ---min=-5000
+-- ---max=5000
+-- ---step=0.1
+-- local track_x = 300
+--
+-- ---$track:Y
+-- ---min=-5000
+-- ---max=5000
+-- ---step=0.1
+-- local track_y = 0
 
----$track:Y
----min=-5000
----max=5000
----step=0.1
-local track_y = 0
+--NOTE: track0、track1じゃないとsetanchorが壊れる
+
+--track0:X,-5000,5000,300
+--track1:Y,-5000,5000,0
 
 ---$track:回転
 ---min=-3600
@@ -24,8 +29,8 @@ local track_rotation = 0
 local track_scale_percent = 100
 
 obj.setanchor("track", 0, "line")
-repeater_dx = obj.getvalue("track.track_x")
-repeater_dy = obj.getvalue("track.track_y")
+repeater_dx = obj.getvalue(0)
+repeater_dy = obj.getvalue(1)
 repeater_dr = track_rotation
 repeater_dk = track_scale_percent * 0.01
 repeater_rep = 0
