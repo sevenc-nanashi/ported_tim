6396335680fa0d50f5197c059a7685e9aee4e22c
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-03-12 19:14:59 +0900

    feat: 泡を動作確認

--- lua/Bubble/泡.obj.lua
+++ lua/Bubble/泡.obj.lua
@@ -23,47 +23,62 @@ local track_direction = 0
 ---step=0.1
 local track_adjust = 50
 
----$value:サイズ
-local size = 100
+---$track:サイズ
+---min=0
+---max=1000
+---step=0.1
+local track_size = 100
 
 ---$color:色
 local col = 0xffffff
 
----$value:ゆらぎ幅
-local fw = 2
+---$track:ゆらぎ幅
+---min=0
+---max=10
+---step=0.1
+local track_fluctuation_width = 2
 
----$value:ゆらぎ速度
-local f_speed = 2
+---$track:ゆらぎ速度
+---min=-50
+---max=50
+---step=0.1
+local track_fluctuation_speed = 2
 
----$value:奥行き
-local depth = 15
+---$track:奥行き
+---min=0
+---max=100
+---step=0.1
+local track_depth = 15
 
----$value:事後エフェクト
-local af = 0
+---$check:事後エフェクト
+local check_after_effect = false
 
----$value:表示領域補正
-local er = 1
+---$track:表示領域補正
+---min=0.1
+---max=10
+---step=0.1
+local track_display_area_correction = 1
 
-obj.load("figure", "円", col, size)
-obj.effect("ぼかし", "範囲", size)
-obj.effect("クリッピング", "右", size * 1.4 * track_adjust / 100)
-obj.effect("クリッピング", "左", size * 1.4 * track_adjust / 100)
+obj.load("figure", "円", col, track_size)
+obj.effect("ぼかし", "範囲", track_size)
+obj.effect("クリッピング", "右", track_size * 1.4 * track_adjust / 100)
+obj.effect("クリッピング", "左", track_size * 1.4 * track_adjust / 100)
 obj.effect("斜めクリッピング")
 obj.effect("極座標変換")
 
-w = obj.screen_w * er / 2 + obj.w
-h = obj.screen_h * er / 2 + obj.h
-if af == 0 then
+w = obj.screen_w * track_display_area_correction / 2 + obj.w
+h = obj.screen_h * track_display_area_correction / 2 + obj.h
+if not check_after_effect then
     obj.effect()
 else
-    obj.setoption("dst", "tmp", obj.screen_w * er, obj.screen_h * er)
+    obj.setoption("dst", "tmp", obj.screen_w * track_display_area_correction, obj.screen_h * track_display_area_correction)
 end
 s = track_speed / 10
 sp = track_direction * obj.w
 s = s * obj.time
-fs = f_speed * obj.time * math.pi * 2 / 10
+fluctuation_speed = track_fluctuation_speed * obj.time * math.pi * 2 / 10
 n = track_count
-fw = fw * obj.w
+fluctuation_width = track_fluctuation_width * obj.w
 xs = -w
 xe = w
 if sp < 0 then
@@ -72,16 +87,16 @@ else
     xs = xs - sp
 end
 for i = 1, n do
-    sc = 1 / (1 + depth * i / n)
+    sc = 1 / (1 + track_depth * i / n)
     t = s * sc + obj.rand(0, 1000, i, 1000) / 1000
     p = math.floor(t)
     t = t - p
-    r = fs + math.pi * 2 * obj.rand(0, 1000, i, 1001) / 1000
-    x = obj.rand(xs, xe, i, p) + (math.sin(r) * fw + t * sp) * sc
+    r = fluctuation_speed + math.pi * 2 * obj.rand(0, 1000, i, 1001) / 1000
+    x = obj.rand(xs, xe, i, p) + (math.sin(r) * fluctuation_width + t * sp) * sc
     y = t * h * 2 - h
     obj.draw(x, y, 0, sc)
 end
 
-if af ~= 0 then
+if check_after_effect then
     obj.load("tempbuffer")
 end
