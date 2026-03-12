702fb9fb6876fc5aa2b8eaf3030e340f18244e5b
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-03-13 00:28:52 +0900

    feat: ルミナンスTを動作確認

--- lua/SceChg/ルミナンスT.lua
+++ lua/SceChg/ルミナンスT.lua
@@ -3,12 +3,12 @@
 ---min=0
 ---max=100
 ---step=0.1
-local rename_me_track0 = 10
+local blur = 10
 
 ---$check:透過反転
-local rename_me_check0 = true
+local invert = true
 
-local blur = 4096 * rename_me_track0 * 0.01
+local blur = 4096 * blur * 0.01
 local T = obj.getvalue("scenechange")
 local L = (4096 + 2 * blur) * T - blur
 
@@ -17,7 +17,7 @@ obj.setoption("drawtarget", "tempbuffer")
 obj.setoption("blend", "alpha_sub")
 
 obj.effect("単色化")
-if rename_me_check0 then
+if invert then
     obj.effect("反転", "輝度反転", 1)
 end
 obj.effect("ルミナンスキー", "基準輝度", L, "ぼかし", blur, "type", 1)
