c7736dddfd5672a942108fe818bdbb7a527a77eb
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-03-12 09:21:52 +0900

    feat: な〜さりいらいむ風TA.anmを移植

--- lua/k4-NsLime/な〜さりい☆らいむ風TA.anm.lua
+++ lua/k4-NsLime/な〜さりい☆らいむ風TA.anm.lua
@@ -5,13 +5,13 @@
 ---step=0.1
 local track_time_ms = 1000
 
----$track:ｻｲｽﾞ％
+---$track:サイズ％
 ---min=5
 ---max=5000
 ---step=0.1
 local track_size_percent = 450
 
----$track:ｻｲｽﾞ誤差
+---$track:サイズ誤差
 ---min=0
 ---max=500
 ---step=0.1
@@ -23,28 +23,36 @@ local track_size_error = 30
 ---step=0.1
 local track_horizontal_offset = 450
 
----$value:中心ズレ
-local cx = 70
+---$track:中心ズレ
+---min=-2000
+---max=2000
+---step=0.1
+local track_center_offset = 70
 
----$value:乱数シード
-local seed = 0
+---$track:乱数シード
+---min=0
+---max=1000000
+---step=1
+local track_random_seed = 0
 
 local T = obj.time
-local Mtime = track_time_ms * 0.001
-local maxsize = track_size_percent * 0.01
-local dsize = track_size_error
-local yz = track_horizontal_offset * 0.01
+local animation_duration = track_time_ms * 0.001
+local max_size = track_size_percent * 0.01
+local size_error = track_size_error
+local horizontal_offset = track_horizontal_offset * 0.01
+local center_offset = track_center_offset or 70
+local random_seed = math.floor(track_random_seed or 0)
 
-local N = obj.num - 1
-local id = obj.index - 1
+local object_count = obj.num - 1
+local object_index = obj.index - 1
 
-local T1 = (N - id) / N * Mtime / 4
-local T2 = T1 + Mtime / 4
-local T3 = id / N * Mtime / 4 + Mtime / 2
-local T4 = id / N * Mtime / 2 + Mtime / 2
+local T1 = (object_count - object_index) / object_count * animation_duration / 4
+local T2 = T1 + animation_duration / 4
+local T3 = object_index / object_count * animation_duration / 4 + animation_duration / 2
+local T4 = object_index / object_count * animation_duration / 2 + animation_duration / 2
 
-local z1 = obj.rand(-dsize, dsize, id, seed) * 0.01
-local z2 = obj.rand(-dsize / 2, dsize / 2, id, 1000 + seed) * 0.01
+local z1 = obj.rand(-size_error, size_error, object_index, random_seed) * 0.01
+local z2 = obj.rand(-size_error / 2, size_error / 2, object_index, 1000 + random_seed) * 0.01
 local x = 1
 
 if T < T1 then
@@ -65,5 +73,5 @@ else
     obj.alpha = 1
 end
 
-obj.zoom = obj.zoom * (1 + (maxsize - 1) * (1 - x))
-obj.ox = obj.ox + yz * (obj.ox - cx) * (1 - x)
+obj.zoom = obj.zoom * (1 + (max_size - 1) * (1 - x))
+obj.ox = obj.ox + horizontal_offset * (obj.ox - center_offset) * (1 - x)
