30ee124318c11f8f9cb4854c6e38f4683d0a9f31
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-03-12 20:32:35 +0900

    feat: 色相環を移植

--- lua/ColAdj6/色相環.obj.lua
+++ lua/ColAdj6/色相環.obj.lua
@@ -21,27 +21,27 @@ local track_lightness = 100
 ---min=0
 ---max=100
 ---step=0.1
-local track_width_percent = 25
+local width_percent = 25
 
----$value:分割
-local spN = 24
+---$track:分割数
+---min=3
+---max=360
+---step=1
+local track_split_count = 24
 
----$value:幅%(旧ﾊﾟﾗﾒｰﾀ)
-local wp = 0
-
-local wp2 = (wp == 0 or track_width_percent > 0) and track_width_percent or wp
+local split_count = math.max(3, math.floor(track_split_count or 24))
 local Ro = track_radius
-local Ri = Ro * (100 - wp2) * 0.01
+local Ri = Ro * (100 - width_percent) * 0.01
 local s = track_saturation
 local v = track_lightness
 obj.setoption("drawtarget", "tempbuffer", 2 * Ro, 2 * Ro)
 obj.setoption("blend", "alpha_add")
-local s2 = (-1 / spN + 2 / 3) * math.pi
+local s2 = (-1 / split_count + 2 / 3) * math.pi
 local x1, y1 = Ro * math.sin(s2), Ro * math.cos(s2)
 local x2, y2 = Ri * math.sin(s2), Ri * math.cos(s2)
-for i = 0, spN do
-    obj.load("figure", "四角形", HSV(360 * i / spN, s, v), 1)
-    local s1 = (-(2 * i - 1) / spN + 2 / 3) * math.pi
+for i = 0, split_count do
+    obj.load("figure", "四角形", HSV(360 * i / split_count, s, v), 1)
+    local s1 = (-(2 * i - 1) / split_count + 2 / 3) * math.pi
     local x0, y0 = Ro * math.sin(s1), Ro * math.cos(s1)
     local x3, y3 = Ri * math.sin(s1), Ri * math.cos(s1)
     obj.drawpoly(x0, y0, 0, x1, y1, 0, x2, y2, 0, x3, y3, 0)
